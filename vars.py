9f161b707ca580cdbcbba1cfb138e40752106675
Radhe2804 <86071003+Radhe2804@users.noreply.github.com>
2025-05-11 19:53:49 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,1 @@
-api_id = 18686102 #change_with_your_Api_id.
-api_hash = "e4a704eeb41be46401fa9993a134781b" #change_with_your_Api_hash.
-bot_token = "7242920191:AAFhkPTs4a8bF5QUUMlrp01wE9tLHnRAPAA" #change_with_your_bot_token.
-auth_users = [5798579221] #change_with_your_user_id.
-sudo_user = 5798579221 #change_with_your_user_id.
-log_channel = --1002513844851 #change_with_your_channel_id.
-txt_channel = --1002513844851 #change_with_your_channel_id.
+
